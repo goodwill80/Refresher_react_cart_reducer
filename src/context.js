b94907ee85740c5eb8317125ae843adf7835b76d
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-09-15 15:01:29 +0800

    added notes for map data structure in context

--- src/context.js
+++ src/context.js
@@ -12,7 +12,38 @@ import {
 
 const AppContext = createContext();
 
-// console.log(cartItems.map((item) => [item.id, item]));
+// **********************************************
+// Map Data Structure Notes
+// a. initialise
+const basket = new Map();
+// b. ADD
+basket.set('apple', { name: 'apples', price: 0.05 });
+basket.set('pear', { name: 'pears', price: 0.05 });
+// c. GET
+const myApples = basket.get('apple');
+const myPears = basket.get('orange');
+// d. CHECK - boolean
+const checkForApples = basket.has('apple');
+// e. SIZE
+const sizwOfBasket = basket.size;
+// f. DELETE
+basket.delete('apple');
+// g. LOOPING
+for (let [key, { name, price }] of basket) {
+  // console.log(key, name, price);
+}
+// h. CONVERTING FROM ARRAY TO MAP
+const items = [
+  { id: 1, name: 'first', price: 10 },
+  { id: 2, name: 'second', price: 12 },
+];
+const itemsMap = new Map(items.map((item) => [item.id, item]));
+// console.log(itemsMap);
+// i. CONVERTING FROM MAP TO ARRAY
+const itemsArray = Array.from(itemsMap.entries());
+console.log(itemsArray.map((item) => item[1]));
+
+// **********************************************
 
 const initialState = {
   loading: false,
@@ -21,7 +52,7 @@ const initialState = {
 
 function AppContextProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const cart = 1;
+
   return (
     <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
